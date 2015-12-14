a0994aeae37cd3f544c848b6f9ac882c977e66fa
Maximiliano Horta <maximilianohorta94@gmail.com>
2015-12-14 22:24:47 +0100

    stated on day7, fixed bitwise part, not the sorting part, should find out where a is and start frem there

---
+++ day7.py
@@ -0,0 +1,13 @@
+f = open("day7.txt")
+lines = f.readlines()
+for line in lines:
+    words = line.split()
+    if 'NOT' in words:
+        words[3] = words[1]
+    elif 'OR' in words:
+        words[4] = words[0] | words[2]
+    elif 'LSHIFT' in words:
+        words[4] = words[0] << words[2]
+    elif 'RSHIFT' in words:
+        words[4] = words[0] << words[2]
+
